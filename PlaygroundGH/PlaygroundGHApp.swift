44bb89e812e92878461b2e86cd5592eee13cabff
Siberian tiger <artliza@gmail.com>
2021-09-20 10:56:56 -0400

    Initial Commit

---
+++ PlaygroundGH/PlaygroundGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundGHApp.swift
+//  PlaygroundGH
+//
+//  Created by Zharikova on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
